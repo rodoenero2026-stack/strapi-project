b3618903728bd3a3e22cbe76cba28e79c9769f17
Rodolfo <rodoenero2026@gmail.com>
2026-03-02 01:45:17 -0600

    a

--- frontend-ecommerce/app/layout.tsx
+++ frontend-ecommerce/app/layout.tsx
@@ -5,7 +5,8 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { ThemeProvider } from "@/components/theme-provide"; 
 import { Toaster } from 'sonner'; 
-import { LoaderProvider } from "@/components/loader-provider"; // Importa el que creamos
+import { LoaderProvider } from "@/components/loader-provider"; 
+import { Suspense } from "react"; // 1. Importamos Suspense
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -22,8 +23,11 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={urbanist.className}>
-        {/* Este es el que dispara la rayita */}
-        <LoaderProvider /> 
+        {/* 2. Envolvemos el LoaderProvider en Suspense */}
+        {/* Esto evita el error de "useSearchParams() should be wrapped in a suspense boundary" al desplegar */}
+        <Suspense fallback={null}>
+          <LoaderProvider /> 
+        </Suspense>
         
         <ThemeProvider
           attribute="class"
